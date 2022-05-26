af89a4629dc25dc036b15dfb5d613591c5156162
Sergio Sanchez <sheko1999@live.com.mx>
2022-05-26 17:12:37 -0500

    error

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "https://pi-dogs-eosin.vercel.app" || "http://localhost:3001";
+const BASE_URL = process.env.CORS_URL || "http://localhost:3001";
 
 export function getDogs() {
   return async function (dispatch) {
