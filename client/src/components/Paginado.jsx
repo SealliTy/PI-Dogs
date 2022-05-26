24fc3f2529eb3c978c4b92db8294125687ce1951
Sergio Sanchez <sheko1999@live.com.mx>
2022-05-25 21:14:35 -0500

    err

--- client/src/components/Paginado.jsx
+++ client/src/components/Paginado.jsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import style from './Paginado.module.css';
+import React from "react";
+import style from "./Paginado.module.css";
 
-export default function Paginado({dogsPerPage, DogsCount, paginado}){
-    let pages = []
+export default function Paginado({ dogsPerPage, DogsCount, paginado }) {
+  let pages = [];
 
-    for (let i= 0; i <= Math.ceil(DogsCount/dogsPerPage); i++) {
-        pages.push(i)
-    }
-    pages.shift()
-    return(
-        <nav className={style.nav}>
-            <ul className={style.paginado_flex}>
-                {
-                    pages && pages.map(n =>(
-                        <li key={n} className={style.paginado_item}>
-                            <button onClick={() => paginado(n)}>{n}</button>
-                        </li>
-                    ))
-                }
-            </ul>
-        </nav>
-    )
+  for (let i = 0; i <= Math.ceil(DogsCount / dogsPerPage); i++) {
+    pages.push(i);
+  }
+  pages.shift();
+  return (
+    <nav className={style.nav}>
+      <ul className={style.paginado_flex}>
+        {pages &&
+          pages?.map((n) => (
+            <li key={n} className={style.paginado_item}>
+              <button onClick={() => paginado(n)}>{n}</button>
+            </li>
+          ))}
+      </ul>
+    </nav>
+  );
 }
