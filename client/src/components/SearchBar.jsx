85d520bc9abf6fe340a87e6b909c9e535dfc816b
Sergio Sanchez <sheko1999@live.com.mx>
2022-05-26 16:45:38 -0500

    error

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,39 +1,38 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { SearchDog } from '../actions/index';
-import style from '../components/SearchBar.module.css';
-import search from '../img/search.svg';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { SearchDog } from "../actions/index";
+import style from "../components/SearchBar.module.css";
+import search from "../img/search.svg";
 
-export default function SearchBar({setPage}) {
-  const [state, setState] = useState('');
+export default function SearchBar({ setPage }) {
+  const [state, setState] = useState("");
   const dispatch = useDispatch();
 
-    const handleChange = (o) => {
-      o.preventDefault();
-      setState(o.target.value);
-    };
-
-    const handleSubmit = (o) => {
+  const handleChange = (o) => {
     o.preventDefault();
-    if (state !== '') {
+    setState(o.target.value);
+  };
+
+  const handleSubmit = (o) => {
+    o.preventDefault();
+    if (state !== "") {
       dispatch(SearchDog(state));
-      setPage(1)
-      setState('');
+      setPage(1);
+      setState("");
     }
   };
 
   return (
-    <form className={style.form} onSubmit={o => handleSubmit(o)}>
+    <form className={style.form} onSubmit={(o) => handleSubmit(o)}>
       <input
         className={style.search}
         type="text"
         placeholder="Search for name..."
         value={state}
-        onChange={o => handleChange(o)}
+        onChange={(o) => handleChange(o)}
       />
 
-      <input type='submit' value='' />
-      <img className={style.icon} src={search} />
+      <input type="submit" value="" />
     </form>
   );
 }
